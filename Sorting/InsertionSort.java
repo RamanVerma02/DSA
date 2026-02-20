5b5658be6ba353f5e6976efb0871fc4ddab89984
Raman verma <raman02verma@gmail.com>
2026-02-20 13:03:09 +0530

    insertion sort modified

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -15,12 +15,32 @@ public class InsertionSort {
             arr[j+1] = key;  // insert at correct position
         }
     }
-    public static void main(String[] args) {
-        int[] arr = {4,1,5,2,3};
-        insertionSort(arr);
-        for(int x :arr){
-            System.out.print(x+" ");
+
+    public static void binarySearch(int[] arr){
+        int key = 4;
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left <= right){
+            int mid = (right+left)/2;
+
+            if(arr[mid] == key) {
+                System.out.println(mid);
+                break;
+            } else if( key > arr[mid]){
+                left = mid +1;
+            } else {
+                right = mid -1;
+            }
         }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        insertionSort(arr);
+        // for(int x :arr){
+        //     System.out.print(x+" ");
+        // }
+        binarySearch(arr);
     }
 }
 
